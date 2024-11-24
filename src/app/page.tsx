e48849f586f2c3fa8cd4b0558708090c6ef1d119
draihan <draihan@student.ubc.ca>
2024-11-24 06:18:46 -0800

    Update project section with new links and descriptions for featured projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="min-h-screen bg-base-100">
@@ -26,8 +24,6 @@ export default function Home() {
               <div data-tip="Docker, Google Cloud, AWS" className="tooltip tooltip-right">
                 deploy.
               </div>
-
-
             </div>
 
             <button className="btn btn-ghost rounded-xl px-8 motion-preset-seesaw-md">
@@ -42,27 +38,64 @@ export default function Home() {
       {/* Featured Work */}
       <section className="py-24 px-6">
         <div className="max-w-6xl mx-auto">
-          <h2 className="text-4xl font-semibold text-center mb-16">Featured Projects</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            <div className="card bg-base-200 shadow-xl">
-              <figure className="px-6 pt-6">
-                <div className="h-64 w-full bg-base-300 rounded-xl"></div>
-              </figure>
+          <h2 className="text-4xl font-semibold text-center mb-16">Projects</h2>
+
+            <div className="flex flex-col md:flex-row justify-center gap-8">
+            <a
+              className="card bg-orange-800 shadow-xl rounded-2xl"
+              href="https://backgone.danyraihan.dev/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <div className="card-body">
-                <h3 className="card-title">Project One</h3>
-                <p>A brief description of your amazing project.</p>
+              <h3 className="card-title">
+                Backgone <span className="font-extralight text-sm">(web app, docker)</span>
+              </h3>
+              <p>Remove everything but you.</p>
               </div>
-            </div>
-            <div className="card bg-base-200 shadow-xl">
-              <figure className="px-6 pt-6">
-                <div className="h-64 w-full bg-base-300 rounded-xl"></div>
-              </figure>
+            </a>
+
+            <a
+              className="card shadow-xl rounded-2xl bg-sky-700"
+              href="https://insightubc.danyraihan.dev/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <div className="card-body">
-                <h3 className="card-title">Project Two</h3>
-                <p>A brief description of your amazing project.</p>
+              <h3 className="card-title">
+                InsightUBC <span className="font-extralight text-sm">(web app, docker)</span>
+              </h3>
+              <p> Query every UBC courses that ever existed.</p>
               </div>
+            </a>
+            <a
+              className="card shadow-xl rounded-2xl bg-green-700"
+              href="https://github.com/Recipict-App/expo-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="card-body">
+              <h3 className="card-title">
+                Recipict<span className="font-extralight text-sm">(mobile app)</span>
+              </h3>
+              <p>Get recipe from grocery bills.</p>
+              </div>
+            </a>
+
+            <a
+              className="card shadow-xl rounded-2xl bg-blue-700"
+              href="https://www.qsitehacks.ca/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="card-body">
+              <h3 className="card-title">
+                qsite<span className="font-extralight text-sm">(web app)</span>
+              </h3>
+              <p>Quantum computing hackathon website.</p>
+              </div>
+            </a>
             </div>
-          </div>
         </div>
       </section>
     </main>
